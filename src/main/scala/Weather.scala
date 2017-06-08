e9ae9dab2b814283c1d41249dc06ac3d0bae13dc
Joald <jacek.olczyk98@gmail.com>
2017-06-08 17:06:55 +0200

    Imported to github

---
+++ src/main/scala/Weather.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by joald_000 on 08-Jun-17.
+  */
+class Weather() {
+  var temperature = 0.0
+  var pressure = 0
+  var cloudiness = 0
+  var humidity = 0
+  var windSpeed = 0.0
+  var windDirection = 0
+
+}
